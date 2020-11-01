73086ae3539f33b6d5a1642cb04a8beca65ed289
Zach Smelcer <zacaronithesmelcer@icloud.com>
2020-11-01 17:00:51 -0600

    Unfortunately I did not commit much on this homework. But, in the last few hours I have completed the assignment. Today was mainly figuring out the front end and making it look burgerish.

--- models/burger.js
+++ models/burger.js
@@ -1,13 +1,12 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-var cat = {
+var burger = {
   all: function(cb) {
     orm.all("burgers", function(res) {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
     orm.create("burgers", cols, vals, function(res) {
       cb(res);
@@ -26,4 +25,4 @@ var cat = {
 };
 
 // Export the database functions for the controller (burgersController.js).
-module.exports = cat;
+module.exports = burger;
